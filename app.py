c7e9c3557d7077bd41d7192e7c2287f8eb094be9
naoki111 <2004240028yn@edu.oca.ac.jp>
2022-06-18 15:01:34 +0900

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 @app.route('/')
 def inex():
@@ -17,4 +17,15 @@ def test_request():
 def exercise_request(user):
     return f'exercise_request:{user}'
 
+@app.route('/show_html')
+def show_html():
+    return render_template('test_html.html')
 
+@app.route('/exercise_html')
+def exercise_html():
+    return render_template('exercise.html')
+
+
+@app.route('/exercise')
+def exercise():
+    return render_template("answer.html", name=request.args.get("my_name"))
